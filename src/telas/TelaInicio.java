d7fc38382979280697dc7ecb2809d955fea7c955
MARCO ANTONIO DE SOUSA MOREIRA <marco.moreira@alunos.sc.senac.br>
2023-04-25 20:58:20 -0300

    Initial commit

---
+++ src/telas/TelaInicio.java
@@ -0,0 +1,26 @@
+package telas;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TelaInicio {
+    private JTextField txtPalavra;
+    private JTextArea textArea1;
+    private JLabel lblPalavra;
+    private JLabel lblTraducao;
+    private JButton traduzirButton;
+    private JButton limparButton;
+    private JPanel telaInicio;
+
+
+
+public TelaInicio() {
+    traduzirButton.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    });
+}
+}
